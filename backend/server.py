f7146e17da82edab1bfb90967022803c0318afac
MAB2005 <your-email@example.com>
2025-12-23 13:33:47 +0530

    enable CORS

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from backend.ai_engine import ask_ai
-app=FastAPI()
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/ask")
-def ask(q:str):
-    return {"reply":ask_ai(q)}
+def ask(q: str):
+    return {"reply": ask_ai(q)}
